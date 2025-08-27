08788e83f69a0b5786f495f3f69c8859a0533dbb
Alexey Lagun <alexey.lagun@gmail.com>
2025-08-27 15:51:28 +0300

    fix: todo - исправлено сохранение в store

--- src/app/providers/rootReducer.ts
+++ src/app/providers/rootReducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { todoApi } from '@/feature/todo/api/todoApi'
-import todoReducer from '@/feature/todo/slice/todoSlice'
 
 export const rootReducer = combineReducers({
   [todoApi.reducerPath]: todoApi.reducer,
-  todos: todoReducer,
 })
